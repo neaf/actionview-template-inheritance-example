f73d66cfc1370a31fcd9d0e6bd58f9427ae84451
Tomasz Werbicki <tomasz@werbicki.net>
2011-10-15 09:46:50 +0200

    Clean Rails application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TemplateInheritanceTest::Application.config.secret_token = 'bbecc469a60ed347918c58e326c6d6b38702d75e04c65b41c15e8bfe3692d48aed7d24d709750a0ba51fb6dae89974e12071109495f513b728ffd21bfd733be8'
